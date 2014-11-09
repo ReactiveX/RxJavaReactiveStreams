8af072d9da3d554300e631c32920bbb56097eff3
Luke Daley <ld@ldaley.com>
2014-11-09 11:27:05 +1000

    Add some coverage for error state observables.

--- rxjava-reactive-streams/src/test/java/rx/reactivestreams/NonTckTest.java
+++ rxjava-reactive-streams/src/test/java/rx/reactivestreams/NonTckTest.java
@@ -19,22 +19,24 @@ import org.reactivestreams.Publisher;
 import org.testng.annotations.Test;
 import rx.Observable;
 import rx.RxReactiveStreams;
+import rx.Subscriber;
 import rx.reactivestreams.test.RsSubscriber;
 import rx.reactivestreams.test.RxSubscriber;
 import rx.reactivestreams.test.IterablePublisher;
 
 import java.util.Arrays;
 
-import static org.testng.Assert.assertNull;
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.*;
 import static org.testng.AssertJUnit.assertEquals;
+import static rx.RxReactiveStreams.toObservable;
+import static rx.RxReactiveStreams.toPublisher;
 
 public class NonTckTest {
 
     @Test
     public void canSubscribeToObservableAsPublisher() {
         Observable<Integer> observable = Observable.just(1, 2, 3);
-        Publisher<Integer> publisher = RxReactiveStreams.toPublisher(observable);
+        Publisher<Integer> publisher = toPublisher(observable);
         RsSubscriber<Integer> subscriber = new RsSubscriber<Integer>();
         publisher.subscribe(subscriber);
 
@@ -50,7 +52,7 @@ public class NonTckTest {
     @Test
     public void canSubscribeToPublisherAsObservable() {
         Publisher<Integer> publisher = new IterablePublisher<Integer>(Arrays.asList(1, 2, 3));
-        Observable<Integer> observable = RxReactiveStreams.toObservable(publisher);
+        Observable<Integer> observable = toObservable(publisher);
         RxSubscriber<Integer> subscriber = new RxSubscriber<Integer>(0);
         observable.subscribe(subscriber);
 
@@ -66,13 +68,48 @@ public class NonTckTest {
     @Test
     public void rxSubscriberNotMakingInitialRequestConumesPublisher() {
         Publisher<Integer> publisher = new IterablePublisher<Integer>(Arrays.asList(1, 2, 3));
-        Observable<Integer> observable = RxReactiveStreams.toObservable(publisher);
+        Observable<Integer> observable = toObservable(publisher);
         RxSubscriber<Integer> subscriber = new RxSubscriber<Integer>(-1); // -1 means no initial request
         observable.subscribe(subscriber);
 
         assertEquals("all items sent", 3, subscriber.received.size());
         assertTrue(subscriber.complete);
         assertNull(subscriber.error);
+    }
+
+    @Test void errorStatePublisherSendsSingleErrorPostSubscribe() {
+        RsSubscriber<Integer> subscriber = new RsSubscriber<Integer>();
+        toPublisher(Observable.<Integer>error(new RuntimeException("!"))).subscribe(subscriber);
+
+        // An error state observable always allows subscription, but then immediately sends onError.
+        // The spec suggests not trying to subscribe but immediately firing onError without an onSubscribe.
+        // However, this isn't a spec violation.
+        assertNotNull(subscriber.subscription);
+
+        assertFalse(subscriber.complete);
+        assertEquals(subscriber.error.getClass(), RuntimeException.class);
+        assertEquals(subscriber.error.getMessage(), "!");
+    }
+
+    @Test void rxFailingOnSubscribeSendsSingleErrorPostSubscribe() {
+        RsSubscriber<Integer> subscriber = new RsSubscriber<Integer>();
+        Observable<Integer> error = Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                throw new RuntimeException("!");
+            }
+        });
+
+        toPublisher(error).subscribe(subscriber);
+
+        // An error state observable always allows subscription, but then immediately sends onError.
+        // The spec suggests not trying to subscribe but immediately firing onError without an onSubscribe.
+        // However, this isn't a spec violation.
+        assertNotNull(subscriber.subscription);
+
+        assertFalse(subscriber.complete);
+        assertEquals(subscriber.error.getClass(), RuntimeException.class);
+        assertEquals(subscriber.error.getMessage(), "!");
     }
 
 }
